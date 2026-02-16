e6de9cb6641e66d999261a69daa531bd0e90d87f
KASSIOCOSTA <benilsonkassio069@gmail.com>
2026-02-15 22:39:42 -0300

    Initial commit

---
+++ musica.js
@@ -0,0 +1,44 @@
+const audio = document.querySelector('#audio');
+const barra = document.querySelector('#barra');
+const btn_Play_Pause = document.querySelector('#btn-play-pause');
+const atual = document.querySelector('#atual');
+const duracao = document.querySelector('#duracao');
+
+btn_Play_Pause.addEventListener('click', ()=>{
+    if(audio.paused ===true){
+        audio.play()
+        btn_Play_Pause.innerHTML = '||'
+    }
+    else{
+        audio.pause()
+        btn_Play_Pause.innerHTML = '&#x25B6;'
+        
+    }
+
+}) // o play e o pause da musica
+
+
+audio.addEventListener('loadedmetadata',()=>{
+    barra.max = audio.duration
+    duracao.innerHTML = formatarTempo(audio.duration)
+}) // quando o audio carregar vai pegar o tanto de segundos e transformar
+
+
+audio.addEventListener('timeupdate',()=>{
+    barra.value = audio.currentTime
+    atual.innerHTML = formatarTempo(audio.currentTime)
+}) // atualizar a barra enquanto o audio toca
+
+audio.addEventListener('ended', () => {
+    btn_Play_Pause.innerHTML = '&#x25B6;';
+})
+
+barra.addEventListener('input',()=>{
+    audio.currentTime = barra.value
+}) // ao mexer na barra fica com a quantidade no audio
+
+function formatarTempo(segundos){
+    const min = Math.floor(segundos / 60)
+    const seg = Math.floor(segundos % 60)
+      return `${min < 10 ? '0' + min : min}:${seg < 10 ? '0' + seg : seg}`
+}
